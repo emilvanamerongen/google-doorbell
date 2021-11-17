ee7991bd87199645f47a7f7f1513ff512911109f
emilvanamerongen <emil.vanamerongen@vartion.com>
2021-11-17 20:30:46 +0100

    pushnotifier support

--- doorbell.py
+++ doorbell.py
@@ -1,4 +1,5 @@
 from googlehomepush import GoogleHome
+from pushnotifier import PushNotifier as pn
 import time 
 import os
 from dotenv import load_dotenv
@@ -9,10 +10,10 @@ import sys
 load_dotenv() 
 
 # define env variables
-HOSTS = os.getenv('HOSTS', '192.168.8.220').split(',')
+HOSTS = os.getenv('HOSTS', '').split(',')
 SOUND_URL = os.getenv('SOUND_URL', "https://www.myinstants.com/media/sounds/movie_1.mp3")
 GPIO_PIN = os.getenv('GPIO_PIN', 15)
-
+NOTIFIER = pn.PushNotifier(os.getenv('PUSHNOTIFIER_USER', ''), os.getenv('PUSHNOTIFIER_PASSWORD', ''), os.getenv('PUSHNOTIFIER_PACKAGE', ''),  os.getenv('PUSHNOTIFIER_TOKEN'))
 
 print('starting service')
 print(HOSTS)
@@ -25,15 +26,18 @@ def playSound(host):
     except:
         pass
 
-def playSounds(args):
+def notify(args):
     print('DING')
-    for host in HOSTS:
-        thread = Thread(target = playSound, args=[host])
-        thread.start()
+    if os.getenv('PUSHNOTIFIER_USER', ''):
+        NOTIFIER.send_text('ding dong')
+    if HOSTS:
+        for host in HOSTS:
+            thread = Thread(target = playSound, args=[host])
+            thread.start()
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(GPIO_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.add_event_detect(GPIO_PIN, GPIO.RISING, callback=playSounds)
+GPIO.add_event_detect(GPIO_PIN, GPIO.RISING, callback=notify)
 
 while True:
     time.sleep(10)

3108d14c7cae6fb6f883a01462a3217a13048041
emilvanamerongen <emil.vanamerongen@vartion.com>
2021-11-17 21:50:24 +0100

    comments

--- doorbell.py
+++ doorbell.py
@@ -5,18 +5,21 @@ import os
 from dotenv import load_dotenv
 from threading import Thread
 import RPi.GPIO as GPIO
-import sys
 
 load_dotenv() 
+## define env variables
 
-# define env variables
+# Raspberry Pi GPIO pin that senses for HIGH signal
+GPIO_PIN = os.getenv('GPIO_PIN', 15)
+
+# Hosts to send a sound to when triggered
 HOSTS = os.getenv('HOSTS', '').split(',')
 SOUND_URL = os.getenv('SOUND_URL', "https://www.myinstants.com/media/sounds/movie_1.mp3")
-GPIO_PIN = os.getenv('GPIO_PIN', 15)
-NOTIFIER = pn.PushNotifier(os.getenv('PUSHNOTIFIER_USER', ''), os.getenv('PUSHNOTIFIER_PASSWORD', ''), os.getenv('PUSHNOTIFIER_PACKAGE', ''),  os.getenv('PUSHNOTIFIER_TOKEN'))
 
-print('starting service')
-print(HOSTS)
+# Login to PushNotifier for push notifications
+if os.getenv('PUSHNOTIFIER_USER', ''):
+    NOTIFIER = pn.PushNotifier(os.getenv('PUSHNOTIFIER_USER', ''), os.getenv('PUSHNOTIFIER_PASSWORD', ''), os.getenv('PUSHNOTIFIER_PACKAGE', ''),  os.getenv('PUSHNOTIFIER_TOKEN'))
+
 
 def playSound(host):
     print('DONG')
@@ -28,13 +31,20 @@ def playSound(host):
 
 def notify(args):
     print('DING')
+    # Send notifications to all users on PushNotifier account 
     if os.getenv('PUSHNOTIFIER_USER', ''):
         NOTIFIER.send_text('ding dong')
+
+    # Send play requests to Google devices in threads
     if HOSTS:
         for host in HOSTS:
             thread = Thread(target = playSound, args=[host])
             thread.start()
 
+
+print('Starting service..')
+
+# Setup listen event on GPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(GPIO_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.add_event_detect(GPIO_PIN, GPIO.RISING, callback=notify)
